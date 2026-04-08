874985b6c215a22833a302d05f4e3cba7cc8b8eb
mateusalbano <134537815+mateusalbano@users.noreply.github.com>
2026-04-08 15:37:21 -0300

    Mais alguns tratamentos na limpeza de dados.

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -2,6 +2,26 @@ import pandas as pd
 import numpy as np
 import os
 
+MAX_MISSINGNESS_THRESHOLD = 5 # Percentage threshold for dropping columns
+
+
+def get_column_data_missingness(df):
+    missingness = df.isnull().mean() * 100
+    return missingness.sort_values(ascending=False)
+
+def drop_highly_missing_columns(df):
+    missingness = get_column_data_missingness(df)
+    columns_to_drop = missingness[missingness > MAX_MISSINGNESS_THRESHOLD].index
+    return df.drop(columns=columns_to_drop)
+
+def fill_missing_numeric_values(df):
+    for col in df.columns:
+        if df[col].dtype in [np.float64, np.int64]:
+            df[col] = df[col].fillna(df[col].median())
+        # else:
+        #     df[col] = df[col].fillna(df[col].mode()[0])
+    return df
+
 
 def _format_date(df):
     return pd.to_datetime(
@@ -18,6 +38,7 @@ def clean_emdat_data(df):
 
     mandatory_cols = ['DisNo.', 'ISO', 'Classification Key', 'Start Year']
     df = df.dropna(subset=mandatory_cols)
+
     
     df['Start Month'] = df['Start Month'].fillna(1).astype(int)
     df['Start Day'] = df['Start Day'].fillna(1).astype(int)
@@ -28,8 +49,10 @@ def clean_emdat_data(df):
         df[col] = df[col].fillna(0)
     
     df['Total Affected'] = df['Total Affected'].fillna(_get_total_affected(df))
-    
     df['External IDs'] = df['External IDs'].str.split('|')
+
+    df = drop_highly_missing_columns(df)
+    df = fill_missing_numeric_values(df)
     
     return df
 
